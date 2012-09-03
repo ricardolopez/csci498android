238eda8cbc792b18596876bb40ebdd06a992ace7
Ricardo Lopez <rlopez@mymail.mines.edu>
2012-09-03 00:31:29 -0600

    Created a ListView for the restaurants

--- LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
+++ LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
@@ -1,16 +1,22 @@
 package csci498.rlopez.lunchlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity {
-	Restaurant r = new Restaurant();
+	List<Restaurant> model = new ArrayList<Restaurant>();
+	ArrayAdapter<Restaurant> adapter = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,15 @@ public class MainActivity extends Activity {
         
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
+        
+        ListView list = (ListView)findViewById(R.id.restaurants);
+        adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, model);
+        list.setAdapter(adapter);
     }
     
     private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
+			Restaurant r = new Restaurant();
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.addr);
 			
@@ -33,7 +44,8 @@ public class MainActivity extends Activity {
 			r.setAddress(address.getText().toString());
 			
 			RadioGroup types = (RadioGroup)findViewById(R.id.types);
-			setDeliveryType(types.getCheckedRadioButtonId());
+			setDeliveryType(types.getCheckedRadioButtonId(), r);
+			adapter.add(r);
 		}
 	};
 
@@ -52,21 +64,21 @@ public class MainActivity extends Activity {
 		take_out.setId(1);
 		delivery.setText("Delivery");
 		delivery.setId(2);
-		one.setText("One");
-		two.setText("Two");
-		three.setText("Three");
-		four.setText("Four");
+		//one.setText("One");
+		//two.setText("Two");
+		//three.setText("Three");
+		//four.setText("Four");
 		
 		types.addView(sit_down);
 		types.addView(take_out);
 		types.addView(delivery);
-		types.addView(one);
-		types.addView(two);
-		types.addView(three);
-		types.addView(four);
+		//types.addView(one);
+		//types.addView(two);
+		//types.addView(three);
+		//types.addView(four);
 	}
 	
-	private void setDeliveryType(int type) {
+	private void setDeliveryType(int type, Restaurant r) {
 		switch (type) {
 		case 0:
 			r.setType("sit_down");
