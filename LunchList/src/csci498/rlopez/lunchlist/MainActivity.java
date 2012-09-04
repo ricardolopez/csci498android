22d20cb28f4f935c5bd1624ed902ec7bbbfaa0e8
Ricardo Lopez <rlopez@mymail.mines.edu>
2012-09-03 21:01:07 -0600

    Added ball_colors and changed the text color with each new restaurant type

--- LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
+++ LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
@@ -5,20 +5,25 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	List<Restaurant> model = new ArrayList<Restaurant>();
-	ArrayAdapter<Restaurant> adapter = null;
+	RestaurantAdapter adapter = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,12 +36,12 @@ public class MainActivity extends Activity {
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
         
-        Spinner spinner = (Spinner)findViewById(R.id.restaurants);
-        adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, model);
-        spinner.setAdapter(adapter);
+        ListView list = (ListView)findViewById(R.id.restaurants);
+        adapter = new RestaurantAdapter();
+        list.setAdapter(adapter);
         
-        AutoCompleteTextView autoComplete = (AutoCompleteTextView)findViewById(R.id.addr);
-    	autoComplete.setAdapter(adapter);
+        //AutoCompleteTextView autoComplete = (AutoCompleteTextView)findViewById(R.id.addr);
+    	//autoComplete.setAdapter(adapter);
     }
     
     private View.OnClickListener onSave = new View.OnClickListener() {
@@ -103,5 +108,60 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+    	RestaurantAdapter() {
+    		super(MainActivity.this, android.R.layout.simple_list_item_1, model);
+    	}
+    	
+    	public View getView(int position, View convertView, ViewGroup parent) {
+    		View row = convertView;
+    		RestaurantHolder holder = null;
+    		
+    		if (row == null) {
+    			LayoutInflater inflater = getLayoutInflater();
+    			row = inflater.inflate(R.layout.row, null);
+    			holder = new RestaurantHolder(row);
+    			row.setTag(holder);
+    		} else {
+    			holder = (RestaurantHolder)row.getTag();
+    		}
+    		
+    		holder.populateFrom(model.get(position));
+    		
+    		return(row);
+    	}
+    }
+    
+    static class RestaurantHolder {
+    	private TextView name = null;
+    	private TextView address = null;
+    	private ImageView icon = null;
+    	
+    	RestaurantHolder(View row) {
+    		name = (TextView)row.findViewById(R.id.title);
+    		address = (TextView)row.findViewById(R.id.address);
+    		icon = (ImageView)row.findViewById(R.id.icon);
+    	}
+    	
+    	void populateFrom(Restaurant r) {
+    		name.setText(r.getName());
+    		address.setText(r.getAddress());
+
+    		if (r.getType().equals("sit_down")) {
+    			icon.setImageResource(R.drawable.ball_red);
+    			name.setTextColor(Color.RED);
+    			address.setTextColor(Color.RED);
+    		} else if (r.getType().equals("take_out")) {
+    			icon.setImageResource(R.drawable.ball_yellow);
+    			name.setTextColor(Color.YELLOW);
+    			address.setTextColor(Color.YELLOW);
+    		} else {
+    			icon.setImageResource(R.drawable.ball_green);
+    			name.setTextColor(Color.GREEN);
+    			address.setTextColor(Color.GREEN);
+    		}
+    	}
     }
 }
