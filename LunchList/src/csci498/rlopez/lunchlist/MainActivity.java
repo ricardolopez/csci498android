207c78a08205f147d2bbb6ca7b7104e8992bd118
Ricardo Lopez <rlopez@mymail.mines.edu>
2012-09-07 11:45:40 -0600

    Added the tab layout, functionality for restaurant click to update the details view

--- LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
+++ LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
@@ -1,3 +1,5 @@
+//COMPLETE APT 6 and reading BCG by FRIDAY
+
 package csci498.rlopez.lunchlist;
 
 import java.util.ArrayList;
@@ -5,33 +7,40 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.TabActivity;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.AdapterView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 	List<Restaurant> model = new ArrayList<Restaurant>();
 	RestaurantAdapter adapter = null;
+	EditText name = null;
+	EditText address = null;
+	RadioGroup types = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = (EditText)findViewById(R.id.name);
+        address = (EditText)findViewById(R.id.addr);
+        types = (RadioGroup)findViewById(R.id.types);
         
-        RadioGroup types = (RadioGroup)findViewById(R.id.types);
-        addTypeButtons(types);
+        //RadioGroup types = (RadioGroup)findViewById(R.id.types);
+        //addTypeButtons(types);
         
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
@@ -39,6 +48,21 @@ public class MainActivity extends Activity {
         ListView list = (ListView)findViewById(R.id.restaurants);
         adapter = new RestaurantAdapter();
         list.setAdapter(adapter);
+        list.setOnItemClickListener(onListClick);
+        
+        TabHost.TabSpec spec=getTabHost().newTabSpec("tag1");
+        
+        spec.setContent(R.id.restaurants);
+        spec.setIndicator("List", getResources().getDrawable(R.drawable.list));
+        
+        getTabHost().addTab(spec);
+        
+        spec=getTabHost().newTabSpec("tag2");
+        spec.setContent(R.id.details);
+        spec.setIndicator("Details", getResources().getDrawable(R.drawable.restaurant));
+        
+        getTabHost().addTab(spec);
+        getTabHost().setCurrentTab(0);
         
         //AutoCompleteTextView autoComplete = (AutoCompleteTextView)findViewById(R.id.addr);
     	//autoComplete.setAdapter(adapter);
@@ -90,15 +114,15 @@ public class MainActivity extends Activity {
 	
 	private void setDeliveryType(int type, Restaurant r) {
 		switch (type) {
-		case 0:
+		case R.id.sit_down:
 			r.setType("sit_down");
 			break;
 		
-		case 1:
+		case R.id.take_out:
 			r.setType("take_out");
 			break;
 			
-		case 2:
+		case R.id.delivery:
 			r.setType("delivery");
 			break;
 		}
@@ -174,4 +198,23 @@ public class MainActivity extends Activity {
     		}
     	}
     }
+    
+    private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+    	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    		Restaurant r = model.get(position);
+    		
+    		name.setText(r.getName());
+    		address.setText(r.getAddress());
+    		
+    		if (r.getType().equals("sit_down")) {
+    			types.check(R.id.sit_down);
+    		} else if (r.getType().equals("take_out")) {
+    			types.check(R.id.take_out);
+    		} else {
+    			types.check(R.id.delivery);
+    		}
+    		
+    		getTabHost().setCurrentTab(1);
+    	}
+	};
 }
