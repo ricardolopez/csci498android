e4a453a31bc2e4f3eb5db2883f0da1c9b88f5fbf
Ricardo Lopez <rlopez@mymail.mines.edu>
2012-09-24 10:13:20 -0600

    Added the DetailForm class file, changed the Manifest xml to reflect this change, changed onListClick function to start new activity

---
+++ LunchList/src/csci498/rlopez/lunchlist/DetailForm.java
@@ -0,0 +1,12 @@
+package csci498.rlopez.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
--- LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
+++ LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
@@ -15,6 +15,7 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.support.v4.widget.CursorAdapter;
 import android.content.Context;
+import android.content.Intent;
 //import android.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -179,20 +180,8 @@ public class MainActivity extends TabActivity {
     
     private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
     	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    		model.moveToPosition(position);
-    		name.setText(helper.getName(model));
-    		address.setText(helper.getAddress(model));
-    		notes.setText(helper.getNotes(model));
-    		
-    		if (helper.getType(model).equals("sit_down")) {
-    			types.check(R.id.sit_down);
-    		} else if (helper.getType(model).equals("take_out")) {
-    			types.check(R.id.take_out);
-    		} else {
-    			types.check(R.id.delivery);
-    		}
-    		
-    		getTabHost().setCurrentTab(1);
+    		Intent i = new Intent(MainActivity.this, DetailForm.class);
+    		startActivity(i);
     	}
 	};
 	
