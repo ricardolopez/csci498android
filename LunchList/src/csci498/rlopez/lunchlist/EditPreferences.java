3c99dcd5a13c9fe8c10464ea82b31753918b5d96
Ricardo Lopez <rlopez@mymail.mines.edu>
2012-09-26 11:23:50 -0600

    Starting APT 13, added preferences xml, EditPreferences.java, and arrays.xml stubs for later in user sorting of restaurants

---
+++ LunchList/src/csci498/rlopez/lunchlist/EditPreferences.java
@@ -0,0 +1,15 @@
+package csci498.rlopez.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class EditPreferences extends PreferenceActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
