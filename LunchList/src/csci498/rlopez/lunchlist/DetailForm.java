e358517c92d418159da34b7118dc60d31d861e37
Ricardo Lopez <rlopez@mymail.mines.edu>
2012-10-02 13:40:15 -0600

    Added the details_options xml, FeedActivity class, a menu icon for RSS feeds, and the library to parse the GET Requests.

--- LunchList/src/csci498/rlopez/lunchlist/DetailForm.java
+++ LunchList/src/csci498/rlopez/lunchlist/DetailForm.java
@@ -2,17 +2,25 @@ package csci498.rlopez.lunchlist;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailForm extends Activity {
 	
 	EditText name;
+	EditText feed;
 	EditText notes;
 	EditText address;
 	RadioGroup types;
@@ -26,10 +34,11 @@ public class DetailForm extends Activity {
 		setContentView(R.layout.detail_form);
 		
 		helper = new RestaurantHelper(this);
-		name =(EditText)findViewById(R.id.name);
-		address =(EditText)findViewById(R.id.addr);
-		notes =(EditText)findViewById(R.id.notes);
-		types =(RadioGroup)findViewById(R.id.types);
+		name = (EditText)findViewById(R.id.name);
+		address = (EditText)findViewById(R.id.addr);
+		notes = (EditText)findViewById(R.id.notes);
+		types = (RadioGroup)findViewById(R.id.types);
+		feed = (EditText)findViewById(R.id.feed);
 		
 		Button save = (Button)findViewById(R.id.save);
 		save.setOnClickListener(onSave);
@@ -83,14 +92,51 @@ public class DetailForm extends Activity {
 				helper.insert(name.getText().toString(),
 							  address.getText().toString(),
 							  type,
-							  notes.getText().toString());
+							  notes.getText().toString(),
+							  feed.getText().toString());
 			} else {
-				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				helper.update(restaurantId, 
+							  name.getText().toString(), 
+							  address.getText().toString(), 
+							  type, 
+							  notes.getText().toString(),
+							  feed.getText().toString());
 			}
 			
 			finish();
 		}
 	};
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.details_option, menu);
+		
+		return(super.onCreateOptionsMenu(menu));
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.feed) {
+			if (isNetworkAvailable()) {
+				Intent i = new Intent(this, FeedActivity.class);
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			} else {
+				Toast.makeText(this, "Connection currently unavailable", Toast.LENGTH_LONG).show();
+			}
+			
+			return(true);
+		}
+		
+		return(super.onOptionsItemSelected(item));
+	}
+	
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+		
+		return(info != null);
+	}
 	
 	private void load() {
 		
@@ -100,6 +146,7 @@ public class DetailForm extends Activity {
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 		
 		if (helper.getType(c).equals("sit_down")) {
 			types.check(R.id.sit_down);
---
+++ LunchList/src/csci498/rlopez/lunchlist/FeedActivity.java
@@ -0,0 +1,105 @@
+package csci498.rlopez.lunchlist;
+
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
+import org.mcsoxford.rss.RSSReader;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.app.Activity;
+
+public class FeedActivity extends Activity {
+
+	private void goBlooey(Throwable t) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		
+		builder.setTitle("Exception!")
+			   .setMessage(t.toString())
+			   .setPositiveButton("OK", null)
+			   .show();
+	}
+	
+	private static class FeedTask extends AsyncTask<String, Void, Void> {
+		
+		private RSSReader reader = new RSSReader();
+		private Exception e;
+		private FeedActivity activity;
+
+		FeedTask(FeedActivity activity) {
+			attach(activity);
+		}
+		
+		void attach(FeedActivity activity) {
+			this.activity = activity;
+		}
+		
+		void detach() {
+			this.activity = null;
+		}
+		
+		@Override
+		public RSSFeed doInBackground(String... urls) {
+			RSSFeed result;
+			
+			try {
+				result = reader.load(urls[0]);
+			} catch (Exception e) {
+				this.e = e;
+			}
+			
+			return(result);
+		}
+
+		@Override
+		public void onPostExecute(RSSFeed feed) {
+			if (e == null) {
+				activity.setFeed(feed);
+			} else {
+				Log.e("LunchList", "Exception parsing feed", e);
+				activity.goBlooey(e);
+			}
+		}
+	}
+	
+	private class FeedAdapter extends BaseAdapter {
+		RSSFeed feed;
+		
+		FeedAdapter(RSSFeed feed) {
+			super();
+			this.feed = feed;
+		}
+		
+		public int getCount() {
+			return(feed.getItems().size());
+		}
+		
+		public Object getItem(int position) {
+			return(feed.getItems().get(position));
+		}
+		
+		public long getItemId(int position) {
+			return(position);
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			
+			if (row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+				
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+			
+			RSSItem item = (RSSItem)getItem(position);
+			((TextView)row).setText(item.getTitle());
+			
+			return(row);
+		}
+	}
+}
