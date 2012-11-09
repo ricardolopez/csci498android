dfbba9a7894aaea70ea49179a1eb1df01c35ddd8
Ricardo Lopez <rlopez@mymail.mines.edu>
2012-11-09 15:38:14 -0700

    Added a new detail_activity xml and a new DetailFragment java file to supply the original functionality as a fragment

--- LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
+++ LunchList/src/csci498/rlopez/lunchlist/MainActivity.java
@@ -1,183 +1,27 @@
-
-
 package csci498.rlopez.lunchlist;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
-import android.preference.PreferenceManager;
-import android.app.Activity;
-import android.app.ListActivity;
-import android.app.TabActivity;
-import android.database.Cursor;
-import android.graphics.Color;
-//import android.support.v4.widget.CursorAdapter;
-import android.content.Context;
 import android.content.Intent;
-import android.widget.CursorAdapter;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
-import android.widget.Toast;
-import android.view.MenuInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TabHost;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-import android.widget.AdapterView;
-import android.widget.ViewFlipper;
-import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends FragmentActivity 
+					      implements LunchFragment.OnRestaurantListener {
 	
-	Cursor model;
-	RestaurantAdapter adapter;
-	EditText name;
-	EditText address;
-	EditText notes;
-	RadioGroup types;
-	RestaurantHelper helper;
-	SharedPreferences prefs;
-	public final static String ID_EXTRA = "csci498.rlopez.lunchlist._ID";
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        helper = new RestaurantHelper(this);
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        initList();
-        prefs.registerOnSharedPreferenceChangeListener(prefListener);
-        
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	new MenuInflater(this).inflate(R.menu.option, menu);
-    	return(super.onCreateOptionsMenu(menu));
-    }
-    
-    private void initList() {
-    	if (model != null) {
-    		stopManagingCursor(model);
-    		model.close();
-    	}
-    	
-    	model = helper.getAll(prefs.getString("sort_order", "name"));
-        startManagingCursor(model);
-        adapter = new RestaurantAdapter(model);
-        setListAdapter(adapter);
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	if (item.getItemId() == R.id.add) {
-    		startActivity(new Intent(MainActivity.this, DetailForm.class));
-    		
-    		return(true);
-    	} else if (item.getItemId() == R.id.prefs){
-    		startActivity(new Intent(this, EditPreferences.class));
-    		
-    		return(true);
-    	}
-    	
-    	return(super.onOptionsItemSelected(item));
-    }
-    
-    private SharedPreferences.OnSharedPreferenceChangeListener prefListener = 
-    		new SharedPreferences.OnSharedPreferenceChangeListener() {
-				
-    	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-			if (key.equals("sort_order")) {
-				initList();
-			}
-					
-		}
-	};
-    
-    class RestaurantAdapter extends CursorAdapter {
-		RestaurantAdapter(Cursor c) {
-    		super(MainActivity.this, c);
-    	}
-    	
-    	@Override
-    	public void bindView(View row, Context ctxt, Cursor c) {
-    		RestaurantHolder holder = (RestaurantHolder)row.getTag();
-    		
-    		holder.populateFrom(c, helper);
-    	}
-    	
-    	@Override
-    	public View newView(Context ctxt, Cursor c, ViewGroup parent) {
-    		LayoutInflater inflater = getLayoutInflater();
-    		View row = inflater.inflate(R.layout.row, parent, false);
-    		RestaurantHolder holder = new RestaurantHolder(row);
-    		
-    		row.setTag(holder);
-    		
-    		return(row);
-    	}
-    }
-    
-    static class RestaurantHolder {
-    	private TextView name;
-    	private TextView address;
-    	private ImageView icon;
-    	
-    	RestaurantHolder(View row) {
-    		name = (TextView)row.findViewById(R.id.title);
-    		address = (TextView)row.findViewById(R.id.address);
-    		icon = (ImageView)row.findViewById(R.id.icon);
-    	}
-    	
-    	void populateFrom(Cursor c, RestaurantHelper helper) {
-    		name.setText(helper.getName(c));
-    		address.setText(helper.getAddress(c));
-    		
-    		if (helper.getType(c).equals("sit_down")) {
-    			icon.setImageResource(R.drawable.ball_red);
-    			name.setTextColor(Color.RED);
-    			address.setTextColor(Color.RED);
-    		} else if (helper.getType(c).equals("take_out")) {
-    			icon.setImageResource(R.drawable.ball_yellow);
-    			name.setTextColor(Color.YELLOW);
-    			address.setTextColor(Color.YELLOW);
-    		} else {
-    			icon.setImageResource(R.drawable.ball_green);
-    			name.setTextColor(Color.GREEN);
-    			address.setTextColor(Color.GREEN);
-    		}
-    	}
-    }
-    
-    private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
-    	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    		
-    		Intent i = new Intent(MainActivity.this, DetailForm.class);
-    		i.putExtra(ID_EXTRA, String.valueOf(id));
-    		
-    		startActivity(i);
-    	}
-	};
+	public final static String ID_EXTRA="apt.tutorial._ID";
 	
 	@Override
-	public void onDestroy(){
-		super.onDestroy();
+	public void onCreate(Bundle savedInstanceState) {	
+		super.onCreate(savedInstanceState);
 		
-		helper.close();
+		setContentView(R.layout.activity_main);
+		LunchFragment lunch = (LunchFragment)getSupportFragmentManager()
+											.findFragmentById(R.id.lunch);
+		lunch.setOnRestaurantListener(this);
+	}
+	
+	public void onRestaurantSelected(long id) {
+		Intent i=new Intent(this, DetailForm.class);
+		i.putExtra(ID_EXTRA, String.valueOf(id));
+		startActivity(i);
 	}
 }
